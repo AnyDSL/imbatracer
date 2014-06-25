ada3625af21e9300bbfc2ebe5b26ee8a628a2dca
Maximilian Malek <s9mamale@stud.uni-saarland.de>
2014-06-25 18:52:29 +0200

    Initial raytracer framework/math code, more to come later. Does not yet compile (impala structs NYI).

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -16,31 +16,12 @@ struct RenderState
 
 static void fillImageBuf(unsigned *buf, unsigned w, unsigned h, void *statep)
 {
-    /*
-    RenderState *state = (RenderState*)statep;
-	int t = (int)state->dtaccu;
-	// TODO: call into impala instead
-	for(unsigned y = 0; y < h; ++y)
-		for(unsigned x = 0; x < w; ++x)
-			buf[y * w + x] =
-				(0xff << 24) // alpha
-				| (t+(x^y^(rand()&0x40)) << 16) // blue
-				| (y << 8) // green
-				| x; // red
-    */
-
-
     impala_render(buf, w, h, statep);
 }
 
 extern "C" void callbackTest(int x, int y)
 {
     printf("callback: (%d, %d)\n", x, y);
-}
-
-extern "C" int c_rand()
-{
-    return rand();
 }
 
 class TestGui : public SDLGui
@@ -56,7 +37,6 @@ protected:
 
 	virtual void _Update(float dt)
 	{
-		// TODO: adjust to screen size (will crash on resize currently)
 		fillImageBuf(_img->getPtr(), _img->width(), _img->height(), &state);
 		ShowImage(_img);
 		state.dtaccu += dt*20;
