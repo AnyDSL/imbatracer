1a5cf0406d6473286bcb2336815a4054e19c4920
Ralf Jung <post@ralfj.de>
2014-07-10 16:28:08 +0200

    final cube

--- src/main/sceneload.cpp
+++ src/main/sceneload.cpp
@@ -12,4 +12,24 @@ void loadScene(impala::Scene *scene)
     scene->verts[5] = impala::point( 1, -1,  1);
     scene->verts[6] = impala::point( 1,  1, -1);
     scene->verts[7] = impala::point( 1,  1,  1);
+    
+    scene->triVerts = (unsigned*)thorin_malloc(12*3*sizeof(unsigned));
+    unsigned i = 0;
+    scene->triVerts[i++] = 0; scene->triVerts[i++] = 1; scene->triVerts[i++] = 2;
+    scene->triVerts[i++] = 6; scene->triVerts[i++] = 1; scene->triVerts[i++] = 2;
+    
+    scene->triVerts[i++] = 0; scene->triVerts[i++] = 1; scene->triVerts[i++] = 3;
+    scene->triVerts[i++] = 5; scene->triVerts[i++] = 1; scene->triVerts[i++] = 3;
+    
+    scene->triVerts[i++] = 0; scene->triVerts[i++] = 2; scene->triVerts[i++] = 3;
+    scene->triVerts[i++] = 4; scene->triVerts[i++] = 2; scene->triVerts[i++] = 3;
+    
+    scene->triVerts[i++] = 7; scene->triVerts[i++] = 6; scene->triVerts[i++] = 5;
+    scene->triVerts[i++] = 1; scene->triVerts[i++] = 6; scene->triVerts[i++] = 5;
+    
+    scene->triVerts[i++] = 7; scene->triVerts[i++] = 6; scene->triVerts[i++] = 4;
+    scene->triVerts[i++] = 2; scene->triVerts[i++] = 6; scene->triVerts[i++] = 4;
+    
+    scene->triVerts[i++] = 7; scene->triVerts[i++] = 5; scene->triVerts[i++] = 4;
+    scene->triVerts[i++] = 3; scene->triVerts[i++] = 5; scene->triVerts[i++] = 4;
 }
