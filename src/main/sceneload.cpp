4b56e926e804d0918a6d2b55f3e8e77b9885251d
Ralf Jung <post@ralfj.de>
2014-07-10 16:21:29 +0200

    interfave and cubus

--- src/main/sceneload.cpp
+++ src/main/sceneload.cpp
@@ -3,6 +3,13 @@
 
 void loadScene(impala::Scene *scene)
 {
-    scene->verts = thorin_malloc(8*sizeof(impala::Point));
-    
+    scene->verts = (impala::Point*)thorin_malloc(8*sizeof(impala::Point));
+    scene->verts[0] = impala::point(-1, -1, -1);
+    scene->verts[1] = impala::point( 1, -1, -1);
+    scene->verts[2] = impala::point(-1,  1, -1);
+    scene->verts[3] = impala::point(-1, -1,  1);
+    scene->verts[4] = impala::point(-1,  1,  1);
+    scene->verts[5] = impala::point( 1, -1,  1);
+    scene->verts[6] = impala::point( 1,  1, -1);
+    scene->verts[7] = impala::point( 1,  1,  1);
 }
