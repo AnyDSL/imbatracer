35aefc804e4af3702f449d28cb2ee518648f97f7
Richard Membarth <richard.membarth@uni-saarland.de>
2014-11-07 16:34:06 +0100

    Add default main to bench; call impala_update once.

--- src/main/bench.cpp
+++ src/main/bench.cpp
@@ -10,6 +10,7 @@ void run_bench(InitBenchFun initBenchFun, const char *name, unsigned n, int w, i
     std::cout << "Running " << name << std::endl;
 
     impala::State *state = initBenchFun();
+    impala::impala_update(state, 0.0);
 
     unsigned *buf = thorin_new<unsigned>(w*h);
     for (unsigned i = 0; i < n; ++i) {
@@ -25,9 +26,11 @@ void run_bench(InitBenchFun initBenchFun, const char *name, unsigned n, int w, i
 int main(int /*argc*/, char */*argv*/[])
 {
     thorin_init();
+    run_bench(impala::impala_init, "Default main, no GUI: 640x480", 10, 640, 480);
+    run_bench(impala::impala_init, "Default main, no GUI: 2560x1920", 10, 640, 480);
     //run_bench(impala::impala_init_bench1, "Bench 1, Config 1", 10, 640/2, 480/2);
-    run_bench(impala::impala_init_bench1, "Bench 1, Config 2", 10, 640, 480);
-    run_bench(impala::impala_init_bench1, "Bench 1, Config 3", 10, 640*4, 480*4);
+    //run_bench(impala::impala_init_bench1, "Bench 1, Config 2", 10, 640, 480);
+    //run_bench(impala::impala_init_bench1, "Bench 1, Config 3", 10, 640*4, 480*4);
 
-    run_bench(impala::impala_init_bench2, "Bench 2, Config 1", 10, 640*2, 480*2);
+    //run_bench(impala::impala_init_bench2, "Bench 2, Config 1", 10, 640*2, 480*2);
 }
