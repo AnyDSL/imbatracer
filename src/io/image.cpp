0c1c82de4d6b46558ddaa0c5fa2c919a998bdb5c
Richard Membarth <richard.membarth@uni-saarland.de>
2016-01-03 13:04:54 +0100

    Updates for new thorin runtime.

--- src/io/image.cpp
+++ src/io/image.cpp
@@ -6,14 +6,14 @@ namespace rt {
 Image::Image(unsigned w, unsigned h)
 {
     //surface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0); // seems safe to use just 0 everywhere
-    rawmem = thorin_malloc(w*h*4);
+    rawmem = thorin_alloc(0, w*h*4);
     surface = SDL_CreateRGBSurfaceFrom(rawmem, w, h, 32, w*4, 0, 0, 0, 0);
 }
 
 Image::~Image()
 {
     SDL_FreeSurface(surface);
-    thorin_free(rawmem);
+    thorin_release(0, rawmem);
 }
 
 } // end namespace rt
