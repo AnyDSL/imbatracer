4dfeafbe0390320d282200f06229a50760a80569
Richard Membarth <richard.membarth@uni-saarland.de>
2017-02-23 12:01:03 +0100

    Update to new syntax & runtime ...

--- src/main/bench.cpp
+++ src/main/bench.cpp
@@ -1,4 +1,4 @@
-#include <thorin_runtime.h>
+#include <runtime.h>
 #include <core/util.h>
 
 #include "interface.h"
@@ -10,26 +10,25 @@ void run_bench(InitBenchFun initBenchFun, const char *name, unsigned n, int w, i
 {
     std::cout << "Running " << name << std::endl;
 
-    impala::State *state = thorin_new<impala::State>(1);
+    impala::State *state = new impala::State[1];
     {
         rt::Scene scene(&state->scene);
         state->sceneMgr = &scene;
         initBenchFun(state);
 
-        unsigned *buf = thorin_new<unsigned>(w*h);
+        unsigned *buf = new unsigned[w*h];
         for (unsigned i = 0; i < n; ++i) {
             impala_render(buf, w, h, true, state);
         }
-        thorin_free(buf);
+        delete []buf;
     }
-    thorin_free(state);
+    delete []state;
 
     std::cout << std::endl;
 }
 
 int main(int /*argc*/, char */*argv*/[])
 {
-    thorin_init();
     //run_bench(impala::impala_init_bench1, "Bench 1, Config 1", 10, 640/2, 480/2);
     run_bench(impala::impala_init_bench1, "Bench 1, Config 2", 10, 640, 480);
     run_bench(impala::impala_init_bench1, "Bench 1, Config 3", 10, 640*4, 480*4);
